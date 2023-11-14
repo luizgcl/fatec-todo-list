b2907c5ef5054354d6e04a9a8fc9edf16fbad6ed
luizgcl <luiz.leme@madelife.med.br>
2023-11-14 15:12:00 -0300

    feat: create utils

---
+++ js/utils/utils.js
@@ -0,0 +1,17 @@
+export function randomID() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomID = '';
+
+  for (let i = 0; i < 8; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomID += characters.charAt(randomIndex);
+  }
+
+  return randomID;
+}
+
+export function formatDate(dateToFormat) {
+  const date = new Date(dateToFormat);
+
+  return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+}
